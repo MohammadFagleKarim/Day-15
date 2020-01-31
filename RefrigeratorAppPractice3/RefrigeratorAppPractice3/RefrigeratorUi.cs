efba89e3d985a1ab1448387f55291916d9e7e2d0
Mohammad Fazle Karim <mohammadfaglekarim@gmail.com>
2020-01-31 20:34:20 +0600

    RefrigeratorAppAdded

---
+++ RefrigeratorAppPractice3/RefrigeratorAppPractice3/RefrigeratorUi.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; 
+
+namespace RefrigeratorAppPractice3
+{
+    public partial class RefrigeratorUi : Form
+    {
+        public int t;
+
+        public RefrigeratorUi()
+        {
+            t = 0;
+            InitializeComponent();
+        }
+
+        Refrrigerators refrrigerators = new Refrrigerators();
+        //List<double> speed = new List<double>();
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(maxWeightTakeTextBox.Text))
+            {
+                MessageBox.Show("Maximun Speed cannot be empty!");
+                return;
+            }
+
+            refrrigerators.MaxWeight = Convert.ToDouble(maxWeightTakeTextBox.Text);
+
+            t++;
+
+            maxWeightTakeTextBox.Clear();
+
+            //if (t>=1)
+            //{
+            //    refrigerator.MaxWeight = Convert.ToDouble(maxWeightTakeTextBox.Text);
+            //    maxWeightTakeTextBox.Clear();
+            //    t--;
+            //    MessageBox.Show("Saved");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Maximum Wight can take once only");
+            //}
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            if (t >= 1)
+            {
+                if (String.IsNullOrEmpty(itemTextBox.Text))
+                {
+                    MessageBox.Show("Items No. cannot be empty!");
+                    return;
+                }
+
+                if (String.IsNullOrEmpty(weightTextBox.Text))
+                {
+                    MessageBox.Show("Unit Weight cannot be empty!");
+                    return;
+                }
+
+                refrrigerators.Add(Convert.ToInt32(itemTextBox.Text), Convert.ToDouble(weightTextBox.Text));
+                
+                currentWeightTextBox.Text = refrrigerators.CurrentWeight().ToString();
+                remainingWeightTextBox.Text = refrrigerators.RemainingWeight().ToString();
+            }
+            else
+                MessageBox.Show("Create a refrigerator first");
+            
+
+            //int noOfitems = Convert.ToInt32(itemTextBox.Text);
+            //double unitWeight = Convert.ToDouble(weightTextBox.Text);
+            //refrigerator.Weight(noOfitems, unitWeight);
+            //refrigerator.remainingWeight = refrigerator.MaxWeight - refrigerator.currentWeight;
+            //currentWeightTextBox.Text = refrigerator.currentWeight.ToString();
+            //remainingWeightTextBox.Text = refrigerator.remainingWeight.ToString();
+            //currentWeightTextBox.ReadOnly = true;
+            //remainingWeightTextBox.ReadOnly = true;
+
+        }
+    }
+}
---
+++ RefrigeratorAppPractice3/RefrigeratorAppPractice3/Refrrigerators.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrigeratorAppPractice3
+{
+    public class Refrrigerators
+    {
+        public double MaxWeight { set; get; }
+
+        public List<int> items = new List<int>();
+        public List<double> unitWeights = new List<double>();
+
+        public void Add(int itemNo, double unitWeight)
+        {
+            items.Add(itemNo);
+            unitWeights.Add(unitWeight);
+        }
+
+        public double CurrentWeight()
+        {
+            double currentWeight=0;
+            for (int i=0; i<items.Count(); i++)
+            {
+                currentWeight +=( items[i] * unitWeights[i]);
+            }
+            return currentWeight;
+        }
+
+        public double RemainingWeight()
+        {
+            double currentWeight = CurrentWeight();
+            double remainingWeight = MaxWeight - currentWeight;
+            return remainingWeight;
+        }
+
+        public void Validate()
+        {
+            if (CurrentWeight() > MaxWeight)
+            {
+                
+            }
+        }
+
+        //public double Weight(int item, double unitWeight)
+        //{
+        //    currentWeight = Convert.ToDouble(item) * unitWeight;
+
+        //    return currentWeight;
+        //}
+    }
+}
